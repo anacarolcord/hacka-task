b49d6fe1333fcac56580eedcaa16b9bde8bf640c
Gabriel Benini <gabriel.benini2@gmail.com>
2025-11-07 20:30:06 -0300

    Metodo de busca personalizda

--- src/main/java/com/example/gerenciador_tarefas/repository/UsuarioRepository.java
+++ src/main/java/com/example/gerenciador_tarefas/repository/UsuarioRepository.java
@@ -1,9 +1,21 @@
 package com.example.gerenciador_tarefas.repository;
 
 import com.example.gerenciador_tarefas.entity.Usuario;
+import com.example.gerenciador_tarefas.entity.enums.Cargo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.List;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, String > {
     UserDetails findByCpf(String cpf);
+
+    List<Usuario> findByNome(String nome);
+
+    List<Usuario> findByCargo(Cargo cargo);
+
+    List<Usuario> findByEmail(String email);
+
+    List<Usuario> findByAtivo(Boolean ativo);
+
 }
--- src/main/java/com/example/gerenciador_tarefas/service/UsuarioService.java
+++ src/main/java/com/example/gerenciador_tarefas/service/UsuarioService.java
@@ -3,6 +3,7 @@ package com.example.gerenciador_tarefas.service;
 import com.example.gerenciador_tarefas.dto.request.UsuarioRequestDTO;
 import com.example.gerenciador_tarefas.dto.response.UsuarioResponseDTO;
 import com.example.gerenciador_tarefas.entity.Usuario;
+import com.example.gerenciador_tarefas.entity.enums.Cargo;
 import com.example.gerenciador_tarefas.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -26,5 +27,42 @@ public class UsuarioService {
                 .stream()
                 .map(UsuarioResponseDTO::fromEntity)
                 .toList();
+    }
+
+    public List<UsuarioResponseDTO> pesquisaUsuarios(String nome, Cargo cargo, String email, Boolean ativo){
+
+        if (nome != null){
+            return usuarioRepository.findByNome(nome)
+                    .stream()
+                    .map(UsuarioResponseDTO::fromEntity)
+                    .toList();
+        }
+
+        if (cargo != null){
+            return usuarioRepository.findByCargo(cargo)
+                    .stream()
+                    .map(UsuarioResponseDTO::fromEntity)
+                    .toList();
+        }
+
+        if (email != null){
+            return usuarioRepository.findByEmail(email)
+                    .stream()
+                    .map(UsuarioResponseDTO::fromEntity)
+                    .toList();
+        }
+
+        if (ativo != null){
+            return usuarioRepository.findByAtivo(ativo)
+                    .stream()
+                    .map(UsuarioResponseDTO::fromEntity)
+                    .toList();
+        }
+
+        return usuarioRepository.findAll()
+                .stream()
+                .map(UsuarioResponseDTO::fromEntity)
+                .toList();
+        
     }
 }
