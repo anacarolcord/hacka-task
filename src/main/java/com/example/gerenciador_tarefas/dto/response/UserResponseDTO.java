0415286391d87de80cb9b7fb8c73d5d70971a312
lucas <lucasdarosparente@gmail.com>
2025-11-07 17:56:55 -0300

    user responses

---
+++ src/main/java/com/example/gerenciador_tarefas/dto/response/UserResponseDTO.java
@@ -0,0 +1,23 @@
+package com.example.gerenciador_tarefas.dto.response;
+
+import com.example.gerenciador_tarefas.entity.User;
+import com.example.gerenciador_tarefas.entity.enums.Cargo;
+
+public record UserResponseDTO(
+        Long idUser,
+        String nome,
+        Cargo cargo,
+        String cpf,
+
+        String email
+) {
+    public static UserResponseDTO fromEntity(User user){
+        return new UserResponseDTO(
+                user.getIdUser(),
+                user.getNome(),
+                user.getCargo(),
+                user.getCpf(),
+                user.getEmail()
+                );
+    }
+}
