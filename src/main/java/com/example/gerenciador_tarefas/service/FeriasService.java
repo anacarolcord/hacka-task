b5d6285ec72ab4079b49f22990a07f19fcb4a606
4ndrewsec5 <4ndrewsec5@gmail.com>
2025-11-08 07:33:56 -0300

    commit logs feriasService

--- src/main/java/com/example/gerenciador_tarefas/service/FeriasService.java
+++ src/main/java/com/example/gerenciador_tarefas/service/FeriasService.java
@@ -7,6 +7,7 @@ import com.example.gerenciador_tarefas.entity.Ferias;
 import com.example.gerenciador_tarefas.entity.Usuario;
 import com.example.gerenciador_tarefas.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,27 +22,35 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class FeriasService {
     private final UsuarioRepository usuarioRepository;
     private final TarefaService tarefaService;
     private final TaskScheduler taskScheduler;
 
     @Transactional
-    public FeriasResponse criarFerias(FeriasRequestDTO request){
+    public FeriasResponse criarFerias(FeriasRequestDTO request) {
+        log.info("Iniciando criacao de ferias, verificando usuario da requisicao...");
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         Usuario usuario = (Usuario) auth.getPrincipal();
 
+        log.info("Usuario verificado, mapeando Ferias...");
         Ferias ferias = request.toEntity();
+
+        log.info("Ferias mapeada, atribuindo ao Usuario e persistindo no DB...");
         usuario.setFerias(ferias);
         usuarioRepository.save(usuario);
+
+        log.info("Usuario persistido, agendando transferencia de tarefas...");
         agendarTrasferencia(request.idUsuarioReceptor(), usuario.getIdUsuario(), ferias.getDataInicio(), "ferias");
         agendarTrasferencia(usuario.getIdUsuario(), request.idUsuarioReceptor(), ferias.getDataFim(), "fim das ferias");
 
-
+        log.info("Tarefas agendadas, finalizando metodo");
         return FeriasResponse.fromEntity(ferias, usuario);
     }
 
     public List<FeriasResponse> listarTodasFerias() {
+        log.info("Iniciando listagem de ferias");
         return usuarioRepository.findAll()
                 .stream()
                 .filter(usuario -> usuario.getFerias() != null) // pega só quem tem férias cadastradas
@@ -49,15 +58,17 @@ public class FeriasService {
                 .collect(Collectors.toList());
     }
 
-    private void agendarTrasferencia(String idUsuariorecebe, String idUsuarioenvia, LocalDateTime dataFerias, String motivo){
-
+    private void agendarTrasferencia(String idUsuariorecebe, String idUsuarioenvia, LocalDateTime dataFerias, String motivo) {
+        log.info("Iniciando execucao agendada da transferencia de tarefa...");
         Runnable taskDevolucao = () -> {
             tarefaService.transferirTarefa(idUsuariorecebe, idUsuarioenvia, motivo);
         };
-        Instant instanteFerias = dataFerias.atZone(ZoneId.of("America/Sao_Paulo")).toInstant();
 
+        log.info("Agendando devolucao das tarefas...");
+        Instant instanteFerias = dataFerias.atZone(ZoneId.of("America/Sao_Paulo")).toInstant();
         taskScheduler.schedule(taskDevolucao, instanteFerias);
 
+        log.info("Tarefa de devolucao agendada");
     }
 
 
