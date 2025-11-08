0278b3da203df7984f99862ff49f10811bc4a733
lucas <lucasdarosparente@gmail.com>
2025-11-08 02:00:22 -0300

    classe ferias e global exceptions

--- src/main/java/com/example/gerenciador_tarefas/service/TarefaService.java
+++ src/main/java/com/example/gerenciador_tarefas/service/TarefaService.java
@@ -7,157 +7,72 @@ import com.example.gerenciador_tarefas.entity.Usuario;
 import com.example.gerenciador_tarefas.entity.enums.Cargo;
 import com.example.gerenciador_tarefas.exception.AcessoNaoAutorizadoException;
 import com.example.gerenciador_tarefas.exception.TarefaNaoEncontradaException;
-import com.example.gerenciador_tarefas.exception.UserNotFoundException;
-import com.example.gerenciador_tarefas.exception.UsuarioInativoException;
 import com.example.gerenciador_tarefas.repository.TarefaRepository;
-import com.example.gerenciador_tarefas.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class TarefaService {
 
     private final TarefaRepository repository;
-    private final UsuarioRepository usuarioRepository;
 
 
-public TarefaResponseDto salvarTarefa(TarefaRequestDto dados, Usuario usuario){
-    if(usuario.getCargo().equals(Cargo.COLABORADOR)){
-        throw new AcessoNaoAutorizadoException();
+    public TarefaResponseDto salvarTarefa(TarefaRequestDto dados, Usuario usuario) {
+        if (usuario.getCargo().equals(Cargo.COLABORADOR)) {
+            throw new AcessoNaoAutorizadoException();
+        }
+        Tarefa tarefa = dados.toEntity();
+
+        repository.save(tarefa);
+
+        return TarefaResponseDto.fromEntity(tarefa);
     }
-     Tarefa tarefa = dados.toEntity();
 
-     repository.save(tarefa);
+    public TarefaResponseDto atualizarTarefa(TarefaRequestDto dados, String idTarefa, Usuario usuario) {
+        Tarefa tarefa = repository.findById(idTarefa)
+                .orElseThrow(TarefaNaoEncontradaException::new);
 
-     return TarefaResponseDto.fromEntity(tarefa);
-}
+        if (usuario.getAtivo() && usuario.getCargo().equals(Cargo.COLABORADOR)) {
+            tarefa.setStatus(dados.status());
+            tarefa.setTempoUtilizado(dados.tempoUtilizado());
+        }
 
-public TarefaResponseDto atualizarTarefaColaborador(TarefaRequestDto dados, String idTarefa, Usuario usuario){
-    Tarefa tarefa = repository.findById(idTarefa)
-            .orElseThrow(TarefaNaoEncontradaException::new);
+        if (usuario.getAtivo()
+                && (usuario.getCargo().equals(Cargo.GESTOR)
+                || usuario.getCargo().equals(Cargo.COLABORADORRESPONSAVEL))) {
 
-    if( usuario.getAtivo() && !usuario.getFerias()) {
-        tarefa.setStatus(dados.status());
-        tarefa.setTempoUtilizado(dados.tempoUtilizado());
-    }else throw new AcessoNaoAutorizadoException();
+            tarefa.setNome(dados.nome());
+            tarefa.setDescricao(dados.descricao());
+            tarefa.setStatus(dados.status());
+            tarefa.setTempoEstimado(dados.tempoEstimado());
+            tarefa.setUsuario(dados.usuario());
+        }
 
-//mandar pro historico
+        if (usuario.getAtivo()
+                && usuario.getCargo().equals(Cargo.ADMIN)) {
 
-    return TarefaResponseDto.fromEntity(tarefa);
+            tarefa.setNome(dados.nome());
+            tarefa.setDescricao(dados.descricao());
+            tarefa.setStatus(dados.status());
+            tarefa.setTempoEstimado(dados.tempoEstimado());
+            tarefa.setUsuario(dados.usuario());
+            tarefa.setTempoUtilizado(dados.tempoUtilizado());
+        }
 
-}
-
-public TarefaResponseDto atualizarTarefaGestor(TarefaRequestDto dados, String idTarefa, Usuario usuario){
-    Tarefa tarefa = repository.findById(idTarefa)
-            .orElseThrow(()-> new TarefaNaoEncontradaException());
-
-    if(usuario.getAtivo()
-            && !usuario.getFerias()){
-
-        tarefa.setNome(dados.nome());
-        tarefa.setDescricao(dados.descricao());
-        tarefa.setStatus(dados.status());
-        tarefa.setTempoEstimado(dados.tempoEstimado());
-        tarefa.setUsuario(dados.usuario());
-    }
-//mandar pro historico
-    return TarefaResponseDto.fromEntity(tarefa);
-}
-
-public TarefaResponseDto atualizaTarefaAdministrador(TarefaRequestDto dados, String idTarefa, Usuario usuario){
-
-    Tarefa tarefa = repository.findById(idTarefa)
-            .orElseThrow(()-> new TarefaNaoEncontradaException());
-
-
-
-    if(usuario.getAtivo()
-            && !usuario.getFerias()) {
-
-        tarefa.setNome(dados.nome());
-        tarefa.setDescricao(dados.descricao());
-        tarefa.setStatus(dados.status());
-        tarefa.setTempoEstimado(dados.tempoEstimado());
-        tarefa.setUsuario(dados.usuario());
-        tarefa.setTempoUtilizado(dados.tempoUtilizado());
+        return TarefaResponseDto.fromEntity(tarefa);
 
     }
 
-    //mandar pro historico
+    /*
+    public List<TarefaResponseDto> listarTodas(Usuario usuario) {
+        if (usuario.getAtivo() && !usuario.getFerias() && usuario.getCargo().equals(Cargo.GESTOR)) {
+        }
 
-    return TarefaResponseDto.fromEntity(tarefa);
+
+    }
+
+     */
 }
-
-//método que pesquisa todas as tarefas que existem
-//método só pode ser feito por gestor e o gestor só pode pesquisar se estiver ativo e não estiver de férias
-public List<TarefaResponseDto> listarTodasGestor(Usuario usuario) {
-
-    List<TarefaResponseDto> todas = new ArrayList<>();
-
-    if (usuario.getAtivo()
-            && !usuario.getFerias()) {
-
-        todas = repository.findAll()
-                .stream()
-                .map(tarefa -> TarefaResponseDto.fromEntity(tarefa))
-                .collect(Collectors.toList());
-
-    }else throw new AcessoNaoAutorizadoException();
-
-    return todas;
-}
-
-//método retorna apenas as tarefas do usuario que esta fazendo a requisiçao
-public List<TarefaResponseDto> listarTodasUsuario(Usuario usuario){
-
-    List <TarefaResponseDto> tarefasPorUsuario = new ArrayList<>();
-
-    if(usuario.getAtivo() && !usuario.getFerias() ) {
-
-         tarefasPorUsuario = repository.findAllByUsuario(usuario)
-                .stream()
-                .map(tarefa -> TarefaResponseDto.fromEntity(tarefa))
-                .collect(Collectors.toList());
-
-
-    }else throw new AcessoNaoAutorizadoException();
-
-    return tarefasPorUsuario;
-}
-
-//método retorna lista de tarefas de um usuario especifico
-    //método de gestor!!
-public List<TarefaResponseDto> listarTodasPeloIdUsuario(Usuario usuario, String idUsuario){
-
-    List<TarefaResponseDto> tarefasPeloIdUsuario = new ArrayList<>();
-
-    if (usuario.getAtivo() && !usuario.getFerias()) {
-
-        Usuario u = usuarioRepository.findById(idUsuario)
-                .orElseThrow(() -> new UserNotFoundException(idUsuario));
-
-
-        tarefasPeloIdUsuario = repository.findAllByUsuario(usuario)
-                .stream()
-                .map(tarefa -> TarefaResponseDto.fromEntity(tarefa))
-                .collect(Collectors.toList());
-
-    }else throw new UsuarioInativoException();
-
-    return tarefasPeloIdUsuario;
-}
-
-
-}
-
-
-
-
-
-
-
--- src/main/java/com/example/gerenciador_tarefas/service/UsuarioService.java
+++ src/main/java/com/example/gerenciador_tarefas/service/UsuarioService.java
@@ -134,16 +134,6 @@ public class UsuarioService {
         throw new UserNotFoundException(idUsuario);
     }
 
-    public UsuarioResponseDTO atualizarFerias(String idUsuario, UsuarioRequestDTO request){
-        final Optional<Usuario> usuarioOptional = usuarioRepository.findById(idUsuario);
-        if(usuarioOptional.isPresent()){
-            Usuario usuario = usuarioOptional.get();
-            usuario.setFerias(request.ferias());
-            usuarioRepository.save(usuario);
-            return UsuarioResponseDTO.fromEntity(usuario);
-        }
-        throw new UserNotFoundException(idUsuario);
-    }
 
     public UsuarioGestorResponseDTO criarGestor(UsuarioGestorRequestDTO request){
         String senha= new BCryptPasswordEncoder().encode(request.senha());
